9b9cda85e622381dafdf05b9364ae6ab42ea70ef
jaypae95 <jaypae95@naver.com>
2021-08-10 18:31:57 +0900

    baekjoon 6603

---
+++ bfs_dfs/백준_6603_로또.py
@@ -0,0 +1,21 @@
+from sys import stdin
+input = stdin.readline
+
+lotto = []
+
+def dfs(count, start):
+    if count == 6:
+        print(*lotto)
+    
+    for ii in range(start, len(current)):
+        lotto.append(current[ii])
+        dfs(count+1, ii+1)
+        lotto.pop()
+
+while True:
+    current = input().split()
+    if current == ['0']:
+        break
+    current = current[1:]
+    dfs(0, 0)
+    print()
