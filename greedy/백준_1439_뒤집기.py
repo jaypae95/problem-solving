effa4e3748a1fdf9a57ad1109403ce31254b75d5
jaypae95 <jaypae95@naver.com>
2021-08-11 16:29:50 +0900

    baekjoon 1439

---
+++ greedy/백준_1439_뒤집기.py
@@ -0,0 +1,16 @@
+from sys import stdin
+input = stdin.readline
+
+s = input()
+group = []
+
+current = s[0]
+for ss in s[1:]:
+    if ss != current:
+        group.append(1)
+        current = '1' if current == '0' else '0'
+
+if len(group) == 1: # ex) 00000000, 11111111
+    print(0)
+else:
+    print(len(group)//2)
