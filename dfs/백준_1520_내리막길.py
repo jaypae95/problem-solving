79246bf1eae12900949943c0b953dc87f3e1e0f7
jaypae95 <jaypae95@naver.com>
2021-08-13 18:29:33 +0900

    fixed file name

--- dfs/백준_1520_내리막길.py
+++ dfs/백준_1520_내리막길.py
@@ -1,21 +1,32 @@
-from sys import stdin
-input = stdin.readline
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
-lotto = []
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
+my_map = []
+row, column = map(int, input().split())
 
-def dfs(count, start):
-    if count == 6:
-        print(*lotto)
+for _ in range(row):
+    my_map.append(list(map(int, input().split())))
+cache = [[-1 for _ in range(column)] for _ in range(row)]
+
+def dfs(y, x):
+    if y == row-1 and x == column-1:
+        return 1
+    if cache[y][x] != -1:
+        return cache[y][x]
+
+    cache[y][x] = 0
+    for ii in range(4):
+        ny = y + dy[ii]
+        nx = x + dx[ii]
+        if not 0<=nx<column or not 0<=ny<row:
+            continue
+        if my_map[ny][nx] >= my_map[y][x]:
+            continue
+        cache[y][x] += dfs(ny, nx)
     
-    for ii in range(start, len(current)):
-        lotto.append(current[ii])
-        dfs(count+1, ii+1)
-        lotto.pop()
+    return cache[y][x]
 
-while True:
-    current = input().split()
-    if current == ['0']:
-        break
-    current = current[1:]
-    dfs(0, 0)
-    print()
+print(dfs(0, 0))
