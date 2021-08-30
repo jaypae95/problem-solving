74f0f8651f565cb23f5bf6a162bdbfa00477d354
jaypae95 <jaypae95@gmail.com>
2021-08-30 23:35:57 +0900

    baekjoon 9935 add faster way comment

--- stack/백준_9935_문자열폭발.py
+++ stack/백준_9935_문자열폭발.py
@@ -4,11 +4,15 @@ input = stdin.readline
 bomb_string = input().rstrip()
 bomb = list(input().rstrip())
 
+# 주석 해제 시 조금 더 빠름
+# last_character = bomb[-1]
 bomb_len = len(bomb)
 no_bomb = []
 
 for bb in bomb_string:
     no_bomb.append(bb)
+    # if bb != last_character:
+    #     continue
     if no_bomb[-bomb_len:] == bomb:
         for _ in range(0, bomb_len):
             no_bomb.pop()
