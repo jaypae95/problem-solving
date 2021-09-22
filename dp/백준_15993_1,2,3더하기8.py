c896dd781be9ece0df45888586368cca03706b0a
jaypae95 <jaypae95@gmail.com>
2021-09-22 15:27:55 +0900

    baekjoon 15993

---
+++ dp/백준_15993_1,2,3더하기8.py
@@ -0,0 +1,24 @@
+from sys import stdin
+input = stdin.readline
+MAX = 100000
+MOD = 1000000009
+EVEN = 0
+ODD = 1
+
+dp = [None for _ in range(4)]
+dp[0] = [0, 0]
+dp[1] = [0, 1]
+dp[2] = [1, 1]
+dp[3] = [2, 2]
+
+for ii in range(4, MAX + 1):
+    dp.append([
+            (dp[ii-1][ODD] + dp[ii-2][ODD] + dp[ii-3][ODD]) % MOD,
+            (dp[ii-1][EVEN] + dp[ii-2][EVEN] + dp[ii-3][EVEN]) % MOD
+            ])
+
+test_case = int(input())
+
+for _ in range(test_case):
+    current = int(input())
+    print(dp[current][ODD], dp[current][EVEN])
